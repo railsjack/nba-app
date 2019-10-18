c5bd04ac0052fbeee583bb78dee00f497f776e7c
railsjack <jinnahrae@gmail.com>
2019-10-18 23:16:05 +0800

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <View>
+          <Text>Welcome</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+
+export default App;
---
+++ app/store/reducers/user_reducer.js
@@ -0,0 +1,11 @@
+export default (state={}, action) => {
+
+    switch (action.type) {
+        case 'SIGN_IN':
+            return state
+        default:
+            return state
+    }
+
+};
+
